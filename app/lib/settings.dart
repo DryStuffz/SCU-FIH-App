5b70f3e68f97ebbf2aeec193b56603822bf595e9
Abtin Olaee <54949917+bobo247@users.noreply.github.com>
2023-07-05 14:25:27 -0700

    Update settings.dart

--- app/lib/settings.dart
+++ app/lib/settings.dart
@@ -11,45 +11,80 @@ class _SettingsDrawerState extends State<SettingsDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        // Add a ListView to the drawer. This ensures the user can scroll
-        // through the options in the drawer if there isn't enough vertical
-        // space to fit everything.
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              padding: EdgeInsets.all(50),
+      // Add a ListView to the drawer. This ensures the user can scroll
+      // through the options in the drawer if there isn't enough vertical
+      // space to fit everything.
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: [
+          const SizedBox(
+            height: 200.0,
+            child: UserAccountsDrawerHeader(
+              accountName: Text('UserName'),
+              accountEmail: Text('UserEmail@gmail.com'),
+              currentAccountPicture: CircleAvatar(),
               decoration: BoxDecoration(
                 color: Color(0xFF4A4E69),
-                borderRadius:  BorderRadius.vertical(
-                    top: Radius.circular(0),
-                    bottom: Radius.circular(30),
-                  ),
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(0),
+                  bottom: Radius.circular(30),
+                ),
               ),
-              child:Text('Settings', textAlign: TextAlign.center, style: TextStyle(color: Color(0xFFF8F7F7), fontSize: 30,  )), 
-              
             ),
-            ListTile(
-              title: const Text('Item 1'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-            ListTile(
-              title: const Text('Item 2'),
-              onTap: () {
-                // Update the state of the app
-                // ...
-                // Then close the drawer
-                Navigator.pop(context);
-              },
-            ),
-          ],
-        ),
-      );
+          ),
+          ListTile(
+            leading: const Icon(Icons.account_circle),
+            title: const Text('Profile'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.language),
+            title: const Text('Change Language'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.notifications),
+            title: const Text('Notifications'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.accessibility),
+            title: const Text('Accessibility'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.help),
+            title: const Text('Help'),
+            onTap: () {
+              // Update the state of the app
+              // ...
+              // Then close the drawer
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
